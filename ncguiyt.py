18bc943a79d81edd2945be33d4e32809e758db38
tct123 <42028373+tct123@users.noreply.github.com>
2023-08-03 16:40:43 +0200

    Update ncguiyt.py

--- ncguiyt.py
+++ ncguiyt.py
@@ -1,3 +1,7 @@
 from nicegui import ui
+from pytube import YouTube
 ui.label("Hallo Welt")
+url = ui.input(placeholder="Input YouTube-URL")
+def download():
+    yt = YouTube(url=url.value)
 ui.run()
