94e901219d01822e31e7b0bc2f4e0c352952f414
tct123 <42028373+tct123@users.noreply.github.com>
2023-08-13 21:21:45 +0200

    Update ncguiyt.py

--- ncguiyt.py
+++ ncguiyt.py
@@ -3,7 +3,7 @@ from pytube import YouTube
 def download(e):
     yt = YouTube(url=url.value)
     print("Downloading")
-    yt.streams.get_highest_resolution().download(output_path="./")
+    yt.streams.get_highest_resolution().download(output_path="./videos")
 ui.label("Hallo Welt")
 url = ui.input(placeholder="Input YouTube-URL")
 dlbtn = ui.button(text="Download",on_click=download)
