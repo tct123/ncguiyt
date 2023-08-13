37acba520392dbab7ed7e38bfc42975ecddb114b
tct123 <42028373+tct123@users.noreply.github.com>
2023-08-13 23:55:22 +0200

    Update ncguiyt.py

--- ncguiyt.py
+++ ncguiyt.py
@@ -1,9 +1,12 @@
 from nicegui import ui
 from pytube import YouTube
 def download(e):
-    yt = YouTube(url=url.value)
-    print("Downloading")
-    yt.streams.get_highest_resolution().download(output_path="./videos")
+    try:
+        yt = YouTube(url=url.value)
+        print("Downloading")
+        yt.streams.get_highest_resolution().download(output_path="./videos")
+    except:
+        print("Error")
 ui.label("Hallo Welt")
 url = ui.input(placeholder="Input YouTube-URL")
 dlbtn = ui.button(text="Download",on_click=download)
